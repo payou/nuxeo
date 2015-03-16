c9cc683a371ba5d5ec19a522aba96570fc74368f
Benoit Delbosc <bdelbosc@nuxeo.com>
2015-03-16 11:02:04 +0100

    tmp save

--- nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-http-read-only/src/main/java/org/nuxeo/elasticsearch/http/readonly/Main.java
+++ nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-http-read-only/src/main/java/org/nuxeo/elasticsearch/http/readonly/Main.java
@@ -20,9 +20,9 @@
 package org.nuxeo.elasticsearch.http.readonly;
 
 import java.io.IOException;
-import java.net.URI;
 import java.security.Principal;
 
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -35,15 +35,6 @@ import javax.ws.rs.core.UriInfo;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.nuxeo.ecm.core.api.NuxeoPrincipal;
@@ -60,40 +51,19 @@ public class Main extends ModuleRoot {
 
     private static final String DEFAULT_ES_BASE_URL = "http://localhost:9200/";
 
-    public class HttpGetWithEntity extends HttpPost {
-
-        public final static String METHOD_NAME = "GET";
-
-        public HttpGetWithEntity(URI url) {
-            super(url);
-        }
-
-        public HttpGetWithEntity(String url) {
-            super(url);
-        }
-
-        @Override
-        public String getMethod() {
-            return METHOD_NAME;
-        }
-    }
-
     @GET
     @Path("{indices}/{types}/_search")
     @Consumes("application/x-www-form-urlencoded")
     public String searchWithPayLoad(@PathParam("indices") String indices, @PathParam("types") String types,
             @Context UriInfo uriInf, MultivaluedMap<String, String> formParams) throws IOException, JSONException {
-        String url = getSearchUrl(indices, types, uriInf);
-        String payload = formParams.keySet().iterator().next();
         NuxeoPrincipal principal = getPrincipal();
+        String url = getSearchUrl(indices, types, uriInf.getRequestUri().getRawQuery());
+        String payload = formParams.keySet().iterator().next();
         log.warn("Body Search: " + url + " user: " + principal + " payload: " + payload);
-        if (principal == null) {
-            throw new IllegalArgumentException("No principal provided");
-        }
         if (!principal.isAdministrator()) {
             payload = addSecurityFilter(payload, principal);
         }
-        return doGet(url, payload);
+        return HttpClient.get(url, payload);
     }
 
     protected String addSecurityFilter(final String payload, NuxeoPrincipal principal) throws JSONException {
@@ -117,79 +87,62 @@ public class Main extends ModuleRoot {
     @Path("{indices}/{types}/_search")
     public String search(@PathParam("indices") String indices, @PathParam("types") String types, @Context UriInfo uriInf)
             throws IOException {
-        String url = getSearchUrl(indices, types, uriInf);
         NuxeoPrincipal principal = getPrincipal();
+        String url = getSearchUrl(indices, types, uriInf.getRequestUri().getRawQuery());
         log.warn("URI Search: " + url + " user: " + principal);
-        if (principal == null) {
-            throw new IllegalArgumentException("No principal provided");
+        String payload = null;
+        if (!principal.isAdministrator()) {
+            payload = createSecurityFilter(url, principal);
         }
-        if (principal.isAdministrator()) {
-            return doGet(url);
-        }
-        return performUriSearchWithSecurity(url, principal);
+        return HttpClient.get(url, payload);
     }
 
-    protected String performUriSearchWithSecurity(String url, NuxeoPrincipal principal) {
+    protected @NotNull String createSecurityFilter(final String url, final NuxeoPrincipal principal) {
         // TODO: convert URI Search query to a body query
-        throw new IllegalArgumentException("URI Search not implemented for non admin user");
+        throw new IllegalArgumentException("URI Search not implemented for non admin user: " + principal + " " + url);
     }
 
     @GET
     @Path("{indices}/{types}/{documentId: [a-zA-Z0-9\\-]+}")
     public String getDocument(@PathParam("indices") String indices, @PathParam("types") String types,
             @PathParam("documentId") String documentId, @Context UriInfo uriInf) throws IOException {
-        String url = getDocumentUrl(indices, types, documentId, uriInf);
-        log.warn("Get: " + url);
-        return doGet(url);
-    }
-
-    protected String doGet(String url) throws IOException {
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet httpget = new HttpGet(url);
-        try (CloseableHttpResponse response = client.execute(httpget)) {
-            HttpEntity entity = response.getEntity();
-            return entity != null ? EntityUtils.toString(entity) : null;
+        NuxeoPrincipal principal = getPrincipal();
+        String url = getDocumentUrl(indices, types, documentId, uriInf.getRequestUri().getRawQuery());
+        log.warn("Get Document: " + url);
+        if (!principal.isAdministrator()) {
+            return checkAuthorization(url, principal);
         }
+        return HttpClient.get(url);
     }
 
-    protected String doGet(String url, String payload) throws IOException {
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGetWithEntity e = new HttpGetWithEntity(url);
-        StringEntity myEntity = new StringEntity(payload, ContentType.create("application/x-www-form-urlencoded",
-                "UTF-8"));
-        e.setEntity(myEntity);
-        try (CloseableHttpResponse response = client.execute(e)) {
-            HttpEntity entity = response.getEntity();
-            return entity != null ? EntityUtils.toString(entity) : null;
-        }
+    protected String checkAuthorization(String url, NuxeoPrincipal principal) {
+        throw new IllegalArgumentException("get document for non Administrator not yet implemented");
     }
 
-    protected String getSearchUrl(String indices, String types, UriInfo uriInf) {
+    protected String getSearchUrl(String indices, String types, String rawQuery) {
         checkValidIndices(indices);
         checkValidTypes(types);
         String url = getElasticsearchBaseUrl() + "/" + indices + "/" + types + "/_search";
-        String query = uriInf.getRequestUri().getRawQuery();
-        if (query != null) {
-            url += "?" + query;
+        if (rawQuery != null) {
+            url += "?" + rawQuery;
         }
         return url;
     }
 
-    protected String getDocumentUrl(String indices, String types, String documentId, UriInfo uriInf) {
+    protected String getDocumentUrl(String indices, String types, String documentId, String rawQuery) {
         checkValidIndices(indices);
         checkValidTypes(types);
         checkValidDocumentId(documentId);
         String url = getElasticsearchBaseUrl() + "/" + indices + "/" + types + "/" + documentId;
-        String query = uriInf.getRequestUri().getRawQuery();
-        if (query != null) {
-            url += "?" + query;
+        if (rawQuery != null) {
+            url += "?" + rawQuery;
         }
         return url;
     }
 
     protected void checkValidDocumentId(String documentId) {
         if (documentId == null) {
-            throw new IllegalArgumentException("Invalid document id");
+            throw new IllegalArgumentException("Invalthrowid document id");
         }
     }
 
@@ -211,8 +164,11 @@ public class Main extends ModuleRoot {
         return DEFAULT_ES_BASE_URL;
     }
 
-    public NuxeoPrincipal getPrincipal() {
+    public @NotNull NuxeoPrincipal getPrincipal() {
         Principal principal = ctx.getPrincipal();
+        if (principal == null) {
+            throw new IllegalArgumentException("No principal found");
+        }
         return (NuxeoPrincipal) principal;
     }
 
